89b39fb13358fa4269a3d18ddf1cef26402cfac0
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-05-09 19:12:47 +0530

    Updated login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,8 @@
 import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(
+      JSON.parse(localStorage.getItem('user')) || null
+    );
