33410a7e5402164101b009b747e30be92768aab6
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-05-09 19:32:51 +0530

    Updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,24 @@
 import axios from 'axios';
 
-const API_KEY = 'YOUR_TMDB_API_KEY'; // Replace with your API key
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchTrendingMovies = async () => {
   const response = await axios.get(
-    `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
+    `${BASE_URL}/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
   );
   return response.data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await axios.get(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+    `${BASE_URL}/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${query}`
   );
   return response.data.results;
 };
 
 export const fetchMovieDetails = async (movieId) => {
   const response = await axios.get(
-    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`
+    `${BASE_URL}/movie/${movieId}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&append_to_response=videos`
   );
   return response.data;
 };
