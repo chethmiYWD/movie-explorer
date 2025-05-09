41150603c87df9511dcd90bcdab63d3760441813
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-05-09 19:20:08 +0530

    Updated movieCard

--- src/components/Movie/MovieCard.js
+++ src/components/Movie/MovieCard.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+
+const MovieCard = ({ movie }) => {
+    return (
+      <Card sx={{ maxWidth: 345, m: 2 }}>
+        <CardMedia
+          component="img"
+          height="400"
+          image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
