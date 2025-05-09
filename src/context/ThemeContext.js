ccc8bdaaf5fd7d9332268d7b597dd5ecf8d0aac5
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-05-09 19:11:20 +0530

    Updated

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,8 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem('darkMode') === 'true'
+  );
